b3dc14d385b6b5b371aee288ad5ece4dca1f4b99
a.strizhachuk <strizhhh@mail.ru>
2019-11-05 14:41:41 +0300

    init features

--- feature.bat
+++ feature.bat
@@ -1,18 +1,64 @@
 echo off
 chcp 65001
 
+setlocal
+set SRC=./src
+for %%a in (".") do set FEATURE=%%~na
+set RUNNER_STAGE="--dev"
 set RUNNER_ROOT=%CD%
+set RUNNER_V8VERSION=8.3.10
+set GITSYNC_V8VERSION=%RUNNER_V8VERSION%
+set GITSYNC_STORAGE_PATH=./build/storage
+set GITSYNC_WORKDIR=%SRC%
 
 if "%1"=="" (
-    echo "Неизвестный режим запуска"
+    echo "Unknown mode"
     exit /b
 ) else (
+    call git checkout -b "feature/%FEATURE%"
     set MODE=%1
 )
 
-if "%MODE%"=="start" ( call :start )
+if "%MODE%"=="src" (
+    call :src
+    call :createrepo
+    )
+if "%MODE%"=="storage" (
+    if "%2"=="" (
+        set RUNNER_STORAGE-VER=
+    ) else (
+        set RUNNER_STORAGE-VER=%2
+    )
+    call :storage
+    call :createrepo
+)
+if "%MODE%"=="export" (
+   if "%2"=="" (
+        set GITSYNC_STORAGE_USER=Администратор
+        set GITSYNC_STORAGE_PASSWORD=
+    ) else (
+        set GITSYNC_STORAGE_USER=%2
+        set GITSYNC_STORAGE_PASSWORD=%3
+    )
+    call :export
+)
+
+echo "Done: %MODE%""
 exit /b
 
-:start
-call ./tools/init.cmd src %*
+:src
+call runner init-dev --settings .\tools\runner.json --src %SRC% %RUNNER_STAGE%
+exit /b
+
+:storage
+call runner init-dev --settings .\tools\runner.json --storage %RUNNER_STAGE%
+exit /b
+
+:createrepo
+call runner createrepo --settings .\tools\runner.json
+call gitsync set-version 1 %SRC%
+exit /b
+
+:export
+call gitsync s -R
 exit /b
