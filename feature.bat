1a3f6dec953b4bc55fe6f09e1946e75b1bffc9eb
a.strizhachuk <strizhhh@mail.ru>
2020-04-03 14:27:44 +0300

    add init db and upgrade feature

--- feature.bat
+++ feature.bat
@@ -2,9 +2,11 @@ echo off
 chcp 65001
 
 setlocal
+set USER=%DEV1C-USER%
+set PASSWORD=%DEV1C-PASSWORD%
+
 set SRC=./src
 for %%a in (".") do set FEATURE=%%~na
-set RUNNER_STAGE="--dev"
 set RUNNER_ROOT=%CD%
 set RUNNER_V8VERSION=8.3.10
 set GITSYNC_V8VERSION=%RUNNER_V8VERSION%
@@ -20,12 +22,11 @@ if "%1"=="" (
     call git checkout -b "feature/%FEATURE%"
     set MODE=%1
 )
-
-if "%MODE%"=="src" (
+if "%MODE%"=="start" (
     call :src
     call :createrepo
     )
-if "%MODE%"=="storage" (
+if "%MODE%"=="from" (
     if "%2"=="" (
         set RUNNER_STORAGE-VER=
     ) else (
@@ -35,30 +36,43 @@ if "%MODE%"=="storage" (
     call :createrepo
 )
 if "%MODE%"=="export" (
-   if "%2"=="" (
-        set GITSYNC_STORAGE_USER=Администратор
-        set GITSYNC_STORAGE_PASSWORD=
-    ) else (
-        set GITSYNC_STORAGE_USER=%2
-        set GITSYNC_STORAGE_PASSWORD=%3
-    )
+    set GITSYNC_STORAGE_USER=Администратор
+    set GITSYNC_STORAGE_PASSWORD=Администратор
     call :export
 )
+if "%MODE%"=="push" (
+    call git push --set-upstream origin "feature/%FEATURE%"
+)
 
-echo "Done: %MODE%""
+echo "feature done"
 exit /b
 
 :src
-call runner init-dev --settings .\tools\runner.json --src %SRC% %RUNNER_STAGE%
+call runner init-dev --settings .\tools\runner.json --src %SRC%
+call :initDB
 exit /b
 
 :storage
-call runner init-dev --settings .\tools\runner.json --storage %RUNNER_STAGE%
+call runner init-dev --settings .\tools\runner.json --storage
+call :initDB
 exit /b
 
 :createrepo
-call runner createrepo --settings .\tools\runner.json
+call runner createrepo --db-user "Администратор" --settings .\tools\runner.json
 call gitsync set-version 0 %SRC%
+if not "%USER%"=="" (
+    if "%PASSWORD%"=="" (
+        call runner createrepouser --storage-user %USER% --settings .\tools\runner.json
+    ) else (
+        call runner createrepouser --storage-user %USER% --storage-pwd %PASSWORD% --settings .\tools\runner.json
+    )
+)
+exit /b
+
+:initDB
+call oscript .\tools\onescript\Compile.os .\tools\epf\
+call runner run --settings .\tools\runner.json --execute .\tools\epf\InitDB.epf --command "CREATEADMIN"
+call runner run --settings .\tools\runner.json --db-user "Администратор" --execute .\tools\epf\InitDB.epf --command "USERS-FROM-JSON=.\tools\users.json"
 exit /b
 
 :export
